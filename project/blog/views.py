944dfa6cf8d6614987a3806aea4aeebcd62e33ee
Ayush Nanda <ananda_be18@thapar.edu>
2021-04-30 21:50:29 +0530

    creating views for comments using function views and defining urls for them

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from blog.models import Post,Comments
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView) #for class based views
 # Create your views here.
 
+#---------------------------VIEWS FOR POSTS----------------------
 class AboutView(TemplateView):
     template_name = 'about.html'
 
@@ -46,4 +49,52 @@ class DraftListView(LoginRequiredMixin,ListView):
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
     
+
+
+
+# -----------------VIEWS FOR COMMENTS------------------------------
+@login_required
+def post_publish(request,pk):
+    get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+
+@login_required
+def add_comment_to_post(request,pk):
+    post =get_object_or_404(Post,pk=pk)
+    if request.method =='POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+        else:
+            form = CommentForm()
+
+        return render(request,'blog/comment_form.html',{'form': form})
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment =get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
+
+
+
+
+
+
+
+
+
+
 
