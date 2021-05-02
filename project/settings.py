825e192a2ad4c4e3a80c379e4ba2b850586f9701
Ayush Nanda <ananda_be18@thapar.edu>
2021-05-02 16:35:10 +0530

    debug false

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'blog/templates/blog')
 SECRET_KEY = 'django-insecure-8#=h*meqfp6__$-u3m=u^h6a%^0uist1hgw0k$!ac8m3o^2hk+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
